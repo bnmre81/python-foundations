87632a13f2f4766c1dbd0ab6fbb2ffb9f879184e
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2019-12-24 16:34:41 +0300

    This calculates age

--- age_calculator.py
+++ age_calculator.py
@@ -3,7 +3,7 @@ import datetime
 def check_birthdate(year, month, day):
 	birth = datetime.datetime(year, month, day)
 	age = (datetime.datetime.now() - birth)
-	convert_days = int(age.day)
+	convert_days = int(age.days)
 	convert_years = convert_days // 365
 	if convert_years > 0:
 		return True
@@ -13,13 +13,13 @@ def check_birthdate(year, month, day):
 def calculate_age(year, month, day):
 	birth = datetime.datetime(year, month, day)
 	age = (datetime.datetime.now() - birth)
-	convert_days = int(age.day)
+	convert_days = int(age.days)
 	convert_years = convert_days // 365
 	print("You are " + str(convert_years) + " years old!")
 
-year = input("Enter year of birth: ")
-month = input("Enter month of birth: ")
-day = input("Enter day of birth: ")
+year = int(input("Enter year of birth: "))
+month = int(input("Enter month of birth: "))
+day = int(input("Enter day of birth: "))
 
 if check_birthdate(year, month, day) == True:
 	print(calculate_age(year, month, day))
