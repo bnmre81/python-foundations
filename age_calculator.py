b986befde73fff984c953b009fb22f3d7a9706d1
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2019-12-27 11:10:16 +0300

    This calculates age

--- age_calculator.py
+++ age_calculator.py
@@ -1,27 +1,26 @@
 import datetime
 
 def check_birthdate(year, month, day):
-	birth = datetime.datetime(year, month, day)
-	age = (datetime.datetime.now() - birth)
-	convert_days = int(age.days)
-	convert_years = convert_days // 365
-	if convert_years > 0:
-		return True
-	else:
-		return False
+    birth = datetime.datetime(year, month, day)
+    age = (datetime.datetime.now() - birth)
+    convert_days = int(age.days)
+    if convert_days > 0:
+        return True
+    else:
+        return False
 
 def calculate_age(year, month, day):
-	birth = datetime.datetime(year, month, day)
-	age = (datetime.datetime.now() - birth)
-	convert_days = int(age.days)
-	convert_years = convert_days // 365
-	print("You are " + str(convert_years) + " years old!")
+    birth = datetime.datetime(year, month, day)
+    age = (datetime.datetime.now() - birth)
+    convert_days = age.days
+    convert_years = int(convert_days // 365.2422)
+    print("You are " + str(convert_years) + " years old!")
 
 year = int(input("Enter year of birth: "))
 month = int(input("Enter month of birth: "))
 day = int(input("Enter day of birth: "))
 
 if check_birthdate(year, month, day) == True:
-	print(calculate_age(year, month, day))
+    print(calculate_age(year, month, day))
 else:
-	print("The birthdate is invalid!")
+    print("The birthdate is invalid!")
